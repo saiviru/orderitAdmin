d6d2a2d03899891b70a0f816a4b6985c34ecd7df
Sai Kiran Boga <saikiran.boga@sagarsoft.in>
2023-07-04 14:01:59 +0530

    bar chart by hour is completed

--- src/components/Admin/Dashboard/NewChart.jsx
+++ src/components/Admin/Dashboard/NewChart.jsx
@@ -4,79 +4,125 @@ import 'chart.js/auto';
 
 
 
-  
-  const options = {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+
+const options = {
+  responsive: true,
+  scales: {
+    y: {
+      beginAtZero: true,
     },
-  };
-  
+  },
+};
 
-  const MyChart = ({orderss}) => {
-    const [orderData, setOrderData] = useState([]);
-    let dataTotals = [];
-    useEffect(()=>{
-        console.log("the orders history from chart",orderss);
-        if(orderss.length>0){
-            setOrderData(orderss);
+
+const MyChart = ({ orderss }) => {
+  const [orderData, setOrderData] = useState([]);
+  const [data, setData] = useState();
+  let dataTotals = [];
+  let todaysOrders = [];
+  const currentDate = new Date();
+  useEffect(() => {
+    if (orderss && orderss.length > 0) {
+      orderss.map((order) => {
+        const dateObject = new Date(order.createdAt);
+        console.log("the orders history from chart", dateObject);
+        if (dateObject.toDateString() === currentDate.toDateString()) {
+          todaysOrders.push(order);
         }
-    });
+      })
+      setOrderData(todaysOrders);
+    }
+  }, [orderss]);
 
-    useEffect(()=>{
-        orderData.map((order)=>{
-            dataTotals.push(order.totalAmount);
-        })
-    }, [orderData]);
+  useEffect(() => {
+    if (orderData && orderData.length > 0) {
 
-    const data = {
-        labels: ['11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am'],
-        datasets: [
-          {
-            label: 'Day wise sales',
-            data: dataTotals,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
+      const ordersByHour = orderData.reduce((result, order) => {
+        const createdAt = new Date(order.createdAt);
+        const hour = createdAt.getHours();
+        const hour12 = (hour % 12) || 12; 
+
+        if (!result[hour12]) {
+          result[hour12] = {
+            orders: [],
+            totalAmount: 0
+          };
+        }
+
+        result[hour12].orders.push(order);
+        result[hour12].totalAmount += order.totalAmount;
+        return result;
+      }, {});
+
+      // Arrange orders based on labels
+      const arrangedOrders = Rawdata.labels.map(label => {
+        const hour = parseInt(label.replace(/[a-z]/gi, ''), 10);
+
+        const { orders, totalAmount } = ordersByHour[hour] || { orders: [], totalAmount: 0 };
+
+        return {
+          label,
+          orders,
+          totalAmount
+        };
+      });
+
+
+      console.log("the orders by hour:", arrangedOrders)
+      dataTotals = arrangedOrders.map(order => order.totalAmount);
+      Rawdata.datasets[0].data = dataTotals;
+      setData(Rawdata);
+      console.log("the data is to be sent :", data)
+    }
+  }, [orderData]);
+
+
+
+  let Rawdata = {
+    labels: ['11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am'],
+    datasets: [
+      {
+        label: 'Day wise sales',
+        data: [],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
         ],
-      };
-
-    const convertTime = (data) => {
-        const utcDateTime = new Date(data);
-        const istDateTime = utcDateTime.toLocaleString("en-US", {
-          timeZone: "Asia/Kolkata",
-        });
-        const istDate = utcDateTime.toLocaleDateString("en-US", {
-          timeZone: "Asia/Kolkata",
-        });
-        const istTime = utcDateTime.toLocaleTimeString("en-US", {
-          timeZone: "Asia/Kolkata", 
-        });
-        return istTime;
-      };
-    return (
-      <div>
-        <Bar data={data} options={options} />
-      </div>
-    );
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
   };
-  
-  export default MyChart;
-  
+
+  const convertTime = (data) => {
+    const utcDateTime = new Date(data);
+    const istDateTime = utcDateTime.toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    });
+    const istDate = utcDateTime.toLocaleDateString("en-US", {
+      timeZone: "Asia/Kolkata",
+    });
+    const istTime = utcDateTime.toLocaleTimeString("en-US", {
+      timeZone: "Asia/Kolkata",
+    });
+    return istTime;
+  };
+  return (
+    <div>
+      {data ? <Bar data={data} options={options} /> : <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default MyChart;
